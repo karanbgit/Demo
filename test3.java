65552653426199aa2bad271806ea3526f332cfa9
karanbgit <152245654+karanbgit@users.noreply.github.com>
2023-11-28 18:49:59 +0530

    Update test3.java

--- test3.java
+++ test3.java
@@ -2,6 +2,6 @@ class Main
 {
   public static void main (String args[])
   {
-      System.out.print("Hello World I  AM JAVA PROGRAMMING");
+      System.out.println("Hello World I  AM JAVA PROGRAMMING\n\n");
   }
 }
