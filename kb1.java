eca99c3e39d3a4d068b64425bf71b128bb60a207
karanbgit <152245654+karanbgit@users.noreply.github.com>
2023-11-28 19:04:19 +0530

    Update kb1.java

--- kb1.java
+++ kb1.java
@@ -1,6 +1,6 @@
 // Your First Program
 
-class HelloWorld {
+class kb1 {
     public static void main(String[] args) {
         System.out.println("Hello World I AM JAVA PROGRAMMING LANGUAGE...\n"); 
     }
